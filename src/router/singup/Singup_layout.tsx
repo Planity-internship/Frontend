57fb47023b28e523570f938ba2aad6df7263b112
INMD1 <lyw514549@gmail.com>
2025-06-04 20:55:35 +0900

    높이 수정

--- src/router/singup/Singup_layout.tsx
+++ src/router/singup/Singup_layout.tsx
@@ -4,8 +4,8 @@ import SignupForm from "./compment/singup_form";
 function Singup_layout() {
     return (
         <>
-            <div className="grid w-screen h-screen  px-10">
-                <div className="py-40">
+            <div className="grid w-screen h-screen px-10">
+                <div className="py-20">
                     {/*Text*/}
                     <p className="text-5xl font-bold pb-3">회원가입</p>
                     <p className="text-sm font-bold pb-6">환영합니다!<br />회원가입을 위해 필수사항을 입력해주세요.</p>
