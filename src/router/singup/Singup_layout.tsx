1e861439c33cc8b38fadbf626226652300aae58b
INMD1 <lyw514549@gmail.com>
2025-06-04 20:56:11 +0900

    최종본

--- src/router/singup/Singup_layout.tsx
+++ src/router/singup/Singup_layout.tsx
@@ -5,7 +5,7 @@ function Singup_layout() {
     return (
         <>
             <div className="grid w-screen h-screen px-10">
-                <div className="py-20">
+                <div className="py-30">
                     {/*Text*/}
                     <p className="text-5xl font-bold pb-3">회원가입</p>
                     <p className="text-sm font-bold pb-6">환영합니다!<br />회원가입을 위해 필수사항을 입력해주세요.</p>
