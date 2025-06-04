733a1019087e4df885a177719d63d855ee8cc6b3
INMD1 <lyw514549@gmail.com>
2025-06-04 20:34:25 +0900

    PWA 적용

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,44 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+      name: 'Planity', // 설치 배너에 표시되는 이름
+      short_name: 'Planity', // 아이콘 아래에 표시될 이름
+      description: '',
+      theme_color: '#F47C66',
+      lang: 'ko',
+      "icons": [
+        {
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
